ab42369140b82030220a79f2e4a655ea16495697
Organ1sm <ywwithpeople@gmail.com>
2024-06-06 20:47:02 +0800

    add tests for Function traits

--- include/Mirage/Util/FunctionTraits.hpp
+++ include/Mirage/Util/FunctionTraits.hpp
@@ -35,4 +35,106 @@ namespace mirage::util
      */
     template <typename F>
     using function_type_t = typename detail::FunctionType<F>::type;
+
+    namespace detail
+    {
+        template <typename Ret, typename... Args>
+        auto FunctionPointerToType(int, Ret (*)(Args...)) -> Ret (*)(Args...);
+
+        template <typename Ret, typename Class, typename... Args>
+        auto FunctionPointerToType(char, Ret (Class::*)(Args...)) -> Ret (Class::*)(Args...);
+
+        template <typename Ret, typename Class, typename... Args>
+        auto FunctionPointerToType(char, Ret (Class::*)(Args...) const)
+            -> Ret (Class::*)(Args...) const;
+    }    // namespace detail
+
+    /**
+     * @brief get a function pointer type from it's function pointer
+     *
+     * @tparam F
+     */
+    template <auto F>
+    using function_pointer_type_t = decltype(detail::FunctionPointerToType(0, F));
+
+    /**
+     * @brief get a function from it's function pointer
+     */
+    template <auto F>
+    using function_type_from_pointer_t =
+        function_type_t<decltype(detail::FunctionPointerToType(0, F))>;
+
+    namespace detail
+    {
+        template <typename Func>
+        struct BasicFunctionTraits;
+
+        template <typename Ret, typename... Args>
+        struct BasicFunctionTraits<Ret(Args...)>
+        {
+            using args        = TypeList<Args...>;
+            using return_type = Ret;
+        };
+    }    // namespace detail
+
+    /**
+     * @brief extract function info from function type
+     *
+     * @tparam Func
+     */
+    template <typename Func>
+    struct function_traits;
+
+    template <typename Ret, typename... Args>
+    struct function_traits<Ret(Args...)> : detail::BasicFunctionTraits<Ret(Args...)>
+    {
+        using type    = Ret(Args...);
+        using pointer = Ret (*)(Args...);
+
+        static constexpr bool is_member = false;
+        static constexpr bool is_const  = false;
+    };
+
+    template <typename Ret, typename Class, typename... Args>
+    struct function_traits<Ret (Class::*)(Args...)> : detail::BasicFunctionTraits<Ret(Args...)>
+    {
+        using type    = Ret (Class::*)(Args...);
+        using pointer = type;
+
+        static constexpr bool is_member = true;
+        static constexpr bool is_const  = false;
+    };
+
+    template <typename Ret, typename Class, typename... Args>
+    struct function_traits<Ret (Class::*)(Args...) const>
+      : detail::BasicFunctionTraits<Ret(Args...)>
+    {
+        using type    = Ret (Class::*)(Args...) const;
+        using pointer = type;
+
+        static constexpr bool is_member = true;
+        static constexpr bool is_const  = true;
+    };
+
+    namespace detail
+    {
+        template <auto F>
+        struct FunctionPointerTraits : function_traits<function_type_from_pointer_t<F>>
+        { };
+    }    // namespace detail
+
+    /**
+     * @brief extract function info from function pointer
+     */
+    template <auto F>
+    using function_pointer_traits = detail::FunctionPointerTraits<F>;
+
+    /**
+     * @brief check a type is a function or function pointer
+     * @tparam T
+     */
+    template <typename T>
+    constexpr bool is_function_v =
+        std::is_function_v<T> || std::is_member_function_pointer_v<T>;
+
 }    // namespace mirage::util
--- test/Utility/FunctionTraits.cpp
+++ test/Utility/FunctionTraits.cpp
@@ -13,5 +13,69 @@ struct Clazz
 
 TEST_CASE("function pointer type")
 {
-    static_assert(std::is_same_v<function_type_t<globFunc>, void(float, std::string)>);
+    static_assert(
+        std::is_same_v<function_type_from_pointer_t<globFunc>, void(float, std::string)>);
+    static_assert(
+        std::is_same_v<function_pointer_type_t<&Clazz::Foo>, std::string (Clazz::*)()>);
+    static_assert(std::is_same_v<function_pointer_type_t<Clazz::staticFoo>, int &(*)(float)>);
+    static_assert(std::is_same_v<function_pointer_type_t<&Clazz::constFoo>,
+                                 void (Clazz::*)(double) const>);
+}
+
+TEST_CASE("function type from pointer")
+{
+    static_assert(
+        std::is_same_v<function_type_from_pointer_t<globFunc>, void(float, std::string)>);
+    static_assert(
+        std::is_same_v<function_type_from_pointer_t<&Clazz::Foo>, std::string (Clazz::*)()>);
+    static_assert(
+        std::is_same_v<function_type_from_pointer_t<Clazz::staticFoo>, int &(float)>);
+    static_assert(std::is_same_v<function_type_from_pointer_t<&Clazz::constFoo>,
+                                 void (Clazz::*)(double) const>);
+}
+
+TEST_CASE("function pointer traits")
+{
+    SECTION("global function")
+    {
+        using traits = function_pointer_traits<globFunc>;
+        static_assert(std::is_same_v<traits::args, TypeList<float, std::string>>);
+        static_assert(!traits::is_const);
+        static_assert(!traits::is_member);
+        static_assert(std::is_same_v<traits::pointer, void (*)(float, std::string)>);
+        static_assert(std::is_same_v<traits::return_type, void>);
+        static_assert(std::is_same_v<traits::type, void(float, std::string)>);
+    }
+
+    SECTION("member function")
+    {
+        using traits = function_pointer_traits<&Clazz::Foo>;
+        static_assert(std::is_same_v<traits::args, TypeList<>>);
+        static_assert(!traits::is_const);
+        static_assert(traits::is_member);
+        static_assert(std::is_same_v<traits::pointer, std::string (Clazz::*)(void)>);
+        static_assert(std::is_same_v<traits::return_type, std::string>);
+        static_assert(std::is_same_v<traits::type, std::string (Clazz::*)(void)>);
+    }
+
+    SECTION("const member function")
+    {
+        using traits = function_pointer_traits<&Clazz::constFoo>;
+        static_assert(std::is_same_v<traits::args, TypeList<double>>);
+        static_assert(traits::is_const);
+        static_assert(traits::is_member);
+        static_assert(std::is_same_v<traits::pointer, void (Clazz::*)(double) const>);
+        static_assert(std::is_same_v<traits::return_type, void>);
+    }
+
+    SECTION("static function")
+    {
+        using traits = function_pointer_traits<Clazz::staticFoo>;
+        static_assert(std::is_same_v<traits::args, TypeList<float>>);
+        static_assert(!traits::is_const);
+        static_assert(!traits::is_member);
+        static_assert(std::is_same_v<traits::pointer, int &(*)(float)>);
+        static_assert(std::is_same_v<traits::return_type, int &>);
+        static_assert(std::is_same_v<traits::type, int &(float)>);
+    }
 }
